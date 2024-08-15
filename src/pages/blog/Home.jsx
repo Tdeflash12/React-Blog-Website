b59cc012dd58e3d3d5291cfb77225032b20508c4
Abhesh Mandal <abheshmandal249@gmail.com>
2024-08-15 07:41:14 +0545

    Made UI

--- src/pages/blog/Home.jsx
+++ src/pages/blog/Home.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import Navbar from '../../Components/Navbar'
+import Layout from '../../layout/Layout'
+import Card from './components/Card'
 
 const Home = () => {
   return (
-    <div>Home</div>
+  <>
+  <Layout>
+ <div className='flex flex-wrap justify-center space-x-5 mt-4' >
+ <Card />
+ <Card />
+ <Card />
+ <Card />
+ <Card />
+ </div>
+ </Layout>
+  </>
+
   )
 }
 
