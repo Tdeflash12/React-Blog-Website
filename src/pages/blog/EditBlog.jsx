0c3e7f45745442f9fe5b9e127a2b37f35fd70f7f
Abhesh Mandal <abheshmandal249@gmail.com>
2024-08-16 15:20:54 +0545

    Complete UI Link to Tag to link the PAges

---
+++ src/pages/blog/EditBlog.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Form from '../../Components/form/Form'
+
+const EditBlog = ({type}) => {
+   
+  return (
+   <layout>
+    <Form type='Edit'/>
+   </layout>
+  )
+}
+
+export default EditBlog
